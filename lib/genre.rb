50502354c90d52d500c4e8329f33d952e308656a
Anjana Nair <anju740@gmail.com>
2020-01-12 23:14:31 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -12,12 +12,12 @@ class Genre
   end
 
   def songs
-    #Song.all.select { |song| song.genre == self}
+    Song.all.select { |song| song.genre == self}
   end
 
   def artists
-    #songs.map do |song|
-      #song.artist
-    #end
+    songs.map do |song|
+      song.artist
+    end
   end
 end
