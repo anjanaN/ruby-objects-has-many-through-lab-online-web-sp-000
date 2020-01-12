352f72fe6c6ce1eb7674a5fdb6b099b62c252485
Anjana Nair <anju740@gmail.com>
2020-01-12 23:30:36 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,8 +3,11 @@ class Appointment
   @@all = []
 
   def initialize(date, patient, doctor)
-
+    @date = date
+    @patient = patient
+    @doctor = doctor
   end
+
   def self.all
     @@all
   end
