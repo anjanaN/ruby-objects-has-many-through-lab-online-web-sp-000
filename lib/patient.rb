a20266c1d2b3ddfbe72ef848f3523acdd610b15b
Anjana Nair <anju740@gmail.com>
2020-01-12 23:28:53 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -19,4 +19,10 @@ class Patient
     #Appointments.all.select {|appointment| appointment.patient == self}
   end
 
+  def doctors
+    #appointments.map do |appointment|
+      #appointment.doctor
+    #end
+  end
+
 end
